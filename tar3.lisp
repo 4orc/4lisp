d16143fda919131ba80e288a9d8f2f22fefc26dc
Tim Menzies <timm@ieee.org>
2022-10-25 14:12:40 -0400

    save

--- tar3.lisp
+++ tar3.lisp
@@ -15,23 +15,25 @@
 (defun cells (string) (splits string :char #\, :filter #'thing))
 
 (defun with-lines (file fun)
-  (with-open-file (s file) (loop (funcall fun (cells (or (read-line s nil) (return)))))))
+  (with-open-file (s file) 
+    (loop (funcall fun (cells (or (read-line s nil) (return)))))))
 
 (defun show-slots(self)
   (labels ((slots (it) #+clisp (class-slots (class-of it))
-                       #+sbcl (sb-mop:class-slots (class-of it)))
+                       #+sbcl  (sb-mop:class-slots (class-of it)))
            (name (x)   #+clisp (slot-definition-name x)
-                       #+sbcl (sb-mop:slot-definition-name x)))
+                       #+sbcl  (sb-mop:slot-definition-name x)))
     (remove-if (lambda (x) (eq #\_ (char (symbol-name x) 0))) 
                (mapcar #'name (slots self)))))
 
-(defun show (self &optional (str t))
-  (labels ((show1       (y) (format nil ":~(~a~) ~a" y (slot-value self y))))
-    (format str "~a" (cons (type-of self)  (mapcar #'show1 (show-slots self))))))
+(defstruct my-structs)
+(defmethod print-object ((self my-structs) str) 
+  (labels ((show1 (y) (format nil ":~(~a~) ~a" y (slot-value self y))))
+    (format str "~a" (cons (type-of self) (mapcar #'show1 (show-slots self))))))
 
 (defmacro defstruct+ (x &body body) 
-  `(progn (defstruct (,x (:constructor ,(intern (format nil "%MAKE-~a" x)))) ,@body)
-          (defmethod print-object ((self ,x) str) (show self str))))
+  `(defstruct (,x (:include my-structs)
+                  (:constructor ,(intern (format nil "%MAKE-~a" x)))) ,@body))
 
 (defstruct+ fred (k 23) (a 1) (_b 2))
 
