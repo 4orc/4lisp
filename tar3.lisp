030eae838151cba565a9d2ea1cd7d4b2a2f07c50
Tim Menzies <timm@ieee.org>
2022-10-26 00:04:34 -0400

    save

--- tar3.lisp
+++ tar3.lisp
@@ -5,31 +5,32 @@
           (t (let ((z (read-from-string y nil nil))) 
                (if (numberp z) z y))))))
 
-(defun with-lines (file fun)
-  (labels 
-    ((cells (string) (splits string :char #\, :filter #'thing))
-     (splits (str &key (char #\,) (filter #'identity))
-             (loop for start = 0 then (1+ finish)
-                   for        finish = (position char str :start start)
-                   collecting (funcall filter (subseq str start finish))
-                   until      (null finish))))
-    (with-open-file (s file) 
-      (loop (funcall fun (cells (or (read-line s nil) (return))))))))
+(defun splits (str &key (char #\,) (filter #'identity))
+  (loop 
+    for start = 0 then (1+ finish)
+    for        finish = (position char str :start start)
+    collecting (funcall filter (subseq str start finish))
+    until      (null finish)))
+
+(defun cells (string) (splits string :char #\, :filter #'thing))
+
+(defun with-lines (file fn)
+  (with-open-file (s file) (loop (funcall fn (cells (or (read-line s nil) (return)))))))
 
 (defstruct struct)
 (defmethod print-object ((self struct) str)
   (format str "(~a ~{~a~^ ~})" (type-of self)
-          (mapcar (lambda (x) (format nil ":~(~a~) ~a" x (slot-value self x))) 
-                  (show self))))
+     (mapcar (lambda (x) (format nil ":~(~a~) ~a" x (slot-value self x)))
+             (public-slots self))))
 
 (defmacro defstruct+ (x &body body) 
   `(progn 
      (defstruct (,x (:include struct)
                     (:constructor ,(intern (format nil "%MAKE-~a" x)))) ,@body)
-     (defmethod show ((self ,x)) 
+     (defmethod public-slots ((self ,x)) 
        ',(remove-if (lambda (x) (eq #\_ (char (symbol-name x) 0))) 
                     (mapcar (lambda (x) (if (consp x) (car x) x)) body)))))
 
 (defstruct+ fred (k 23) (a 1) (_b 2))
 
-(print (%make-fred))
+(format t "~&~a" (%make-fred))
