e54bb99bd98ddf3d7e914132e0d01edcf407fb1e
Tim Menzies <timm@ieee.org>
2022-10-26 00:27:05 -0400

    save

--- tar3.lisp
+++ tar3.lisp
@@ -1,7 +1,7 @@
 (defun thing (x)
   (let ((y (string-trim '(#\Space #\Tab #\Newline) x)))
-    (cond ((string= y "t") t)
-          ((string= y "nil") nil)
+    (cond ((equal y "t") t)
+          ((equal y "nil") nil)
           (t (let ((z (read-from-string y nil nil))) 
                (if (numberp z) z y))))))
 
@@ -24,13 +24,14 @@
              (public-slots self))))
 
 (defmacro defstruct+ (x &body body) 
-  `(progn 
-     (defstruct (,x (:include struct)
-                    (:constructor ,(intern (format nil "%MAKE-~a" x)))) ,@body)
-     (defmethod public-slots ((self ,x)) 
-       ',(remove-if (lambda (x) (eq #\_ (char (symbol-name x) 0))) 
-                    (mapcar (lambda (x) (if (consp x) (car x) x)) body)))))
+  (let ((slots (remove-if (lambda (x) (eq #\_ (char (symbol-name x) 0))) 
+                          (mapcar (lambda (x) (if (consp x) (car x) x)) body))))
+    `(progn 
+       (defstruct (,x (:include struct)
+                      (:constructor ,(intern (format nil "%MAKE-~a" x)))) ,@body)
+       (defmethod public-slots ((self ,x)) ',slots))))
 
 (defstruct+ fred (k 23) (a 1) (_b 2))
 
 (format t "~&~a" (%make-fred))
+;(with-lines "../data/auto93.csv" #'print)
